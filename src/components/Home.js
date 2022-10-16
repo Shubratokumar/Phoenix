ce6b3b36d373319dd45fa075d208f7b5333f421c
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-10-16 12:27:01 +0600

    production done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Banner from './Banner';
+import Production from './Production';
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Banner />
+            <Production />
         </div>
     );
 };
--- src/components/Production.js
+++ src/components/Production.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import "../styles/production.css";
+import ProductionImg from "../assets/images/production.jpg";
 
 const Production = () => {
     return (
-        <div>
-            
+        <div className='production'>
+            <div className="production-img">
+                <img src={ProductionImg} alt="production-img" />
+            </div>
+            <div className="production-desc">
+                <h1 className='production-heading'>
+                    We are a creative film and photo production company based in New York.
+                </h1>
+                <p>
+                    We are a creative film and photo production company hungry for quality in aesthetics. To create modern recognizable stuff we are working with a strong network of experienced professionals. We set up teams to shape your identity, push your idea and manage the workflow from pre- to post-production.
+                </p>
+                <div className='production-btn'>
+                        <button>
+                            Our Production
+                        </button>
+                </div>
+            </div>            
         </div>
     );
 };
