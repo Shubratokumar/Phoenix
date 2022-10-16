ab4ea782730d0107e0c9e2e3d61d02648fce36a9
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-10-16 11:03:36 +0600

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import "../styles/header.css"
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header'>
+            <div className='logo'>
+                <h1>Phoenixihgroup.com</h1>
+            </div>
+            <div className='links'>
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+                <a href="#marketplace">Marketplace</a>
+            </div>            
         </div>
     );
 };
